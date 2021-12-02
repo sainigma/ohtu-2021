3a613e6440771f928fa994133fd4774794cd9031
sainigma <sainigma@gmail.com>
2021-12-02 16:43:26 +0200

    vko5, tehtävä 3 tehty

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,57 +1,50 @@
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+
+    def add_point(self):
+        self.score = self.score + 1
+
+    def __gt__(self, other):
+        if self.score < other.score:
+            return True
+        return False
+
 class TennisGame:
     def __init__(self, player1_name, player2_name):
-        self.player1_name = player1_name
-        self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.players = [Player(player1_name),Player(player2_name)]
+
+    def get_player(self, player_name):
+        for player in self.players:
+            if player.name == player_name:
+                return player
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
-        else:
-            self.m_score2 = self.m_score2 + 1
+        self.get_player(player_name).add_point()
+
+    def final_battle(self):
+        if not (self.players[0].score >= 4 or self.players[1].score >= 4):
+            return False
+
+        delta_score = abs(self.players[0].score - self.players[1].score)
+        if delta_score == 0:
+            return 'Deuce'
+        
+        sorted_players = sorted(self.players)
+        label = 'Advantage' if delta_score < 2 else "Win for"
+        return f'{label} {sorted_players[0].name}'
+
+    def normal_score(self):
+        labels = ["Love", "Fifteen", "Thirty", "Forty"]
+        player_labels = list(map(lambda player: labels[player.score], self.players))
+        if player_labels[0] == player_labels[1]:
+            return f'{player_labels[0]}-All'
+        return f'{player_labels[0]}-{player_labels[1]}'
 
     def get_score(self):
-        score = ""
-        temp_score = 0
+        final_battle = self.final_battle()
+        if final_battle:
+            return final_battle
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            elif self.m_score1 == 3:
-                score = "Forty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
-        else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
-
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
-
-        return score
+        return self.normal_score()
